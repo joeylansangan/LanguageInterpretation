52c584e0e2486baa395b68c498c315769e92bb6e
Joey Lansangan <joeylansangan@gmail.com>
2019-07-07 17:09:26 -0500

    added package requires

--- liri.js
+++ liri.js
@@ -3,6 +3,28 @@ require("dotenv").config();
 
 // code required to import the keys.js file and store it in a variable
 var keys = require("./keys.js");
+// packege requires..
+var fs = require("fs");
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+
+
 
 // variable that retrieves your spotify information via keys.js
 var spotify = new Spotify(keys.spotify);
+
+function searchSpotify(songName){
+
+  spotify
+  .search({ type: 'track', query: songName, limit: 5}, function(err,data){
+      if (err){
+          return console.log("Error occured: " + err);
+      }
+  })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
