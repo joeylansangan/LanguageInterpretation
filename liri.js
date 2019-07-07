7b49e733e8e62756d36536a688b303c8bb62e738
Joey Lansangan <joeylansangan@gmail.com>
2019-07-07 18:45:24 -0500

    movie api call working

--- liri.js
+++ liri.js
@@ -64,15 +64,25 @@ function searchSpotify(songName){
 // movie function
 
 function searchMovie(movieName){
-    
+    apiKey = "f7e57d25";
+
+    axios.get("http://www.omdbapi.com/?t=" + movieName + "&apikey=" + apiKey)
+    .then(function(movie){
+        console.log("\nTitle: " + movie.data.Title);
+        console.log("Year: " + movie.data.Year);
+        console.log("Spotify preview link: " + element.preview_url);
+        console.log("Album: " + element.album.name);
+        console.log("\n----------------\n")
+    })
 }
 
+searchMovie("shrek");
 
-
-
-
-    //  * The song's name
-
-    //  * A preview link of the song from Spotify
-
-    //  * The album that the song is from
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
